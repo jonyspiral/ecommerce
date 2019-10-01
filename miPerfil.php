7ce9aa95cfb33c131efcc3623e4ed5ea10f2a9d4
jonyspiral <54904501+jonyspiral@users.noreply.github.com>
2019-10-01 03:07:19 -0300

    perfil css mobile

--- miPerfil.php
+++ miPerfil.php
@@ -9,7 +9,6 @@ require_once 'funciones\autoload.php';
 
   if ($_SESSION['avatar']) {
       $avatar =  $_SESSION['avatar'] ;
-
     }else{
     $avatar = 'default.png';
 }
@@ -23,104 +22,94 @@ require_once 'funciones\autoload.php';
         $errorArchivo = 'Formato de archivo invalido';
       } else {
         $nombreArchivo = subirAvatar($_FILES['avatar'], $email);
-
-
       }
         //aca meter todolo del if error=== 0
         $archivo = file_get_contents('database/usuarios.json');
         $usuarios = json_decode($archivo, true);
 
-        var_dump($email);
-        foreach ($usuarios as $key => $usuario) {
+      foreach ($usuarios as $key => $usuario) {
+//aqui es donde encontré al usuario
+                  if ($usuario['email'] == $email ) {
+                    $usuarios[$key]['avatar'] = $nombreArchivo;
 
-                  if (($usuario['email'] == $email ) {
-//aca quiero poner en el array el avatar . me da error.
-               //aqui es donde encontré al usuario
+
               //  $usuario['avatar'] =$nombreArchivo;
-              //file_put_contents('database/usuarios.json', $usuariosJson);
+            $usuariosJson= json_encode($usuarios);
+              file_put_contents('database/usuarios.json', $usuariosJson);
             }
 
         }//aca termina el foreach
+}else {
+  //aca declaro $errores
+  $errores ='Hay un error al subir el archivo '.$_FILES['avatar']['error'];
 }
 
 
  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
 
+     <title>Mi perfil</title>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/home.css">
-    <!---->
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/login.css">
+ <link rel="stylesheet" href="css/home.css">
+ </head>
 
-    <link rel="stylesheet" href="css/user.css">
-
-    <meta charset="utf-8">
-    <title></title>
-  </head>
   <body >
+<?php require_once('partials/header.php') ?>
 <div  class=" containerExt">
-
-
-    <?php require_once('partials/header.php') ?>
-
     <div class="containerExt maxViewport styleLogin">
 
 
       <div class="containerExt">
-        <img class="containerDentro logo"src="img\avatar\<?=$avatar?>" alt="Yo"s style="width: 30%;">
+
+          <div class="containerDentro ">
+            <h1 class="styleTitle center" >Bienvenido! <?=($name.' '.$lastName) ?></h1>
+            <img class="containerDentro  "src="img\avatar\<?=$avatar?>" alt="Yo"s style="width:100% height:100px border-radius: 15px 15px 15px 15px;">
+          </div>
+
+      <form class="" action="miPerfil.php" method="post" enctype="multipart/form-data" style="border-radius: 15px 15px 15px 15px;";>
+          <input type="file" accept="img\avatar\<?=$avatar?>" name="avatar"  class=" borderRadiusUP file-input" id="avatar">
+          <p> <?= (isset($errores) ? $errores : '') ?></p>
+          <input class="center btn-primary borderRadiusUP btnHalf" type="submit" value="2- Enviar imagen" style="margin-bottom: 14px;">
 
 
-        <form class="" action="miPerfil.php" method="post" enctype="multipart/form-data">
-      <input type="file" accept="img\avatar\<?=$avatar?>" name="avatar"  class="file-input" id="avatar">
-
-      <p> <?= (isset($errores['avatar']) ? $errores['avatar'] : '') ?></p>
-      <input class="center btn-primary btn" type="submit" value="Enviar datos" >
-      <?php var_dump($_FILES); ?>
-
-    </form>
-      </div>
-      <div class="containerExt" >
-
-        <h1 class="" >Bienvenido! <?=($name.' '.$lastName) ?></h1>
-
-      </div>
-      <div class="containerExt center">
-
-
-
-        <ul class="lista-menu">
-          <li class="form-control btn-primary btn">
+        <ul class="center">
+          <li class="center btn-primary btn">
             Mis Compras
           </li>
-          <li class="form-control btn-primary btn">
+          <li class="center btn-primary btn">
             Favoritos
           </li>
-          <li class="form-control btn-primary btn">
+          <li class="center btn-primary btn">
             Presupuestos
           </li>
-          <li class="form-control btn-primary btn">
+          <li class="center btn-primary btn">
             Envios
           </li>
-          <li class="form-control btn-primary btn">
+          <li class="center btn-primary btn">
             Mis Datos
           </li>
-          <li class="form-control btn-primary btn">
+          <li class="center btn-primary btn">
             Seguridad
           </li>
         </ul>
 
+      </form>
 
-        </div>
       </div>
 
+      </div>
+</div>
         <?php require_once('partials/footer.php')?>
-        </div>
+
   </body>
 </html>

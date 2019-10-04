8e36007ed176468363273b8406915f14e79855d5
jonyspiral <54904501+jonyspiral@users.noreply.github.com>
2019-10-04 09:10:09 -0300

    producto ccs cambios

--- teleAudio.php
+++ teleAudio.php
@@ -27,31 +27,32 @@
       <h2>Televisores!</h2>
       <img style="margin:auto;cursor: zoom-in; width:100%;" src="https://images.fravega.com/f300/88a4c956ea4c5be6c54a39b168a55d64.png"  >
   </div>
-    <div  class="container Ext"><!--grupoTarjeta-->
-      <div  id="Tarjeta"class="containerRelFloatLeft styleLogin "style=" padding: 2%;">
+    <section  class="containerExt"><!--grupoTarjeta-->
+
+      <article  id="Tarjeta"class="containerRelFloatLeft styleLogin "style=" padding: 2%;">
           <h6 class="center" style=" margin-top: 5%; position: absolute; z-index: 2; width: 100%;">Smart TV 50” 4K Ultra HD TCL L50P65</h6>
           <div class="containerRelFloatLeft"style=" padding: 2%;">
-          <img id="imagenTarjeta" src="https://images.fravega.com/s500/d8b3078dcc3eedcb4547b50ba1dc3a8f.jpg" class="" alt="...">
-            <div class="center" style="position: absolute; width:100%; bottom:3%">
-              <div id ="buttons" class="" >
-                <a href="cart.html" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" ><img src="https://img.icons8.com/ios-filled/50/000000/add-shopping-cart.png"></a>
-                <a href="details.html" target="_blank" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" ><img src="https://img.icons8.com/ios-filled/50/000000/zoom-to-extents.png"></a>
+                <img id="imagenTarjeta" src="https://images.fravega.com/s500/d8b3078dcc3eedcb4547b50ba1dc3a8f.jpg" class="" style=" width: 100%;"alt="...">
 
-          <img id="cuotas" src=img/12cuotas.png alt="12-cuotas-sin-interes">
-        </div>
-              </div>
+
+
+                  <a id= 'buttonCard1' href="cart.html" class=" btn btn-primary buttonCard btn-lg active " role="button" aria-pressed="true"  ><img style="width:80%" src="https://img.icons8.com/ios-filled/50/000000/add-shopping-cart.png"></a>
+                  <a id= 'buttonCard1' href="details.html" target="_blank" class=" btn btn-primary btn-lg active" role="button" aria-pressed="true" background-image:><img style="width:80%" src="https://img.icons8.com/ios-filled/50/000000/zoom-to-extents.png"></a>
+                  <img id="cuotas" src=img/12cuotas.png alt="12-cuotas-sin-interes" style="" >
+
+
+
           </div>
-          <div class="card-body">
 
-              <div class="">
                 <p class="price"><span class="priceOld">$ 39.999</span>$ 32.999</p>
                 <p class="discount"><span >17</span>% Off</p>
-              </div>
+        
                 <p class="card-text">Este tele se la rebanca.!!!</p>
-          </div>
 
-    </div>
-    <div id="Tarjeta" class="">
+  </article><!--termina Tarjeta-->
+
+<!--termina
+      <div  id="Tarjeta"class="containerRelFloatLeft styleLogin "style=" padding: 2%;"><div id="Tarjeta" class="">
         <div class="imagenBotones">
         <img id="imagenTarjeta" src="https://images.fravega.com/s250/5987443294170d201de0e5d4d7202dab.jpg" class="" alt="...">
           <div class="" style="position: absolute; width:100%; bottom:3%">
@@ -71,7 +72,8 @@
               <p class="card-text">Este tele se la banca ++.!!!</p>
         </div>
    </div>
-</main>
+ </div>
+
   <div id="Tarjeta" class="">
       <div class="imagenBotones">
       <img id="imagenTarjeta" src="https://images.fravega.com/s250/1fbf1bfdf688349a2d0a064e92ffbc8c.jpg" class="" alt="...">
@@ -92,10 +94,10 @@
             <p class="card-text">Este tele se la rebanca.!!!</p>
       </div>
 
-</div>
+</div>Tarjeta-->
 
-</div><!-- TERMINA grupoTarjeta -->
-
+</section><!-- TERMINA grupoTarjeta -->
+</main>
   <!-- FOOTER -->
         <?php require_once('partials/footer.php')?>
   </body>

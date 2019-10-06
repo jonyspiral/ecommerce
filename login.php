66b036945e2f7f6fdfdecbaea41870d0c88d947c
jonyspiral <54904501+jonyspiral@users.noreply.github.com>
2019-10-06 19:07:36 -0300

    falta css Register

--- login.php
+++ login.php
@@ -54,49 +54,65 @@ if ($_POST) {
         }
   }
 ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+
+    <title>Logueate</title>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/login.css">
     <meta charset="utf-8">
     <title>Login</title>
   </head>
   <body>
-    <div class="containerExt styleLogin">
+    <div id="padre" class="contPadreFlex">
 
-      <div class="containerDentro">
-        <a id="logo" href="home.php">
-          <img src="img\logo.png" alt="go to home" class="logo" style="width:85px" >
-        </a>
-      </div>
-<div class="containerDentro ">
-  <form class="" action="login.php" method="post" enctype="multipart/form-data">
 
-  <!--<label class="containerDentro"for="email">Email address</label>-->
-  <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" value="<?= $email ?>">
-  <p> <?= (isset($errores['email']) ? $errores['email'] : '') ?></p>
-  <!--<label class="containerDentro" for="contraseña">Contraseña</label>-->
-  <input class="form-control" placeholder="Enter password" type="password" name="password" value="">
+  <div id="main" class="flexCenterH styleLogin" style="flex-wrap: wrap; align-content: center;  padding: 10%;">
 
-  <p><?= (isset($errores['password']) ? $errores['password'] : '') ?></p>
 
-  <input type="checkbox" id="mantener" name="mantener" value="">Mantener logueado.
-  <p><a href="register.php" style="font-size: 2em;">(Registrese aqui si aun no lo hizo.)</a></p>
+          <div id="containerLogo"class=" flexCenterH">
+            <a class="" id="logo" href="index.php">
+              <img src="img\logo.png" alt="go to home" class="logo"  >
+            </a>
+          </div>
+          <div class="flexCenterH ">
+          <form class="" action="login.php" method="post" enctype="multipart/form-data">
 
-  <div class="containerDentro">
-      <button class=" btn-primary btn"  type="submit" >Send</button>
-  </div>
+          <!--<label class="containerDentro"for="email">Email address</label>-->
+          <input type="text" class="formControl" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" value="<?= $email ?>">
+          <p> <?= (isset($errores['email']) ? $errores['email'] : '') ?></p>
+          <!--<label class="containerDentro" for="contraseña">Contraseña</label>-->
+          <input class="formControl" placeholder="Enter password" type="password" name="password" value="">
+
+          <p><?= (isset($errores['password']) ? $errores['password'] : '') ?></p>
+
+          <div class="form-group form-check">
+                <input type="checkbox" name="mantener" class="form-check-input" id="mantener">
+                <label class="form-check-label" for="mantener">Dejarme Conectado</label>
+          </div>
+
+          <div class="form-group form-check">
+              <p><a id= "toRegister" href="register.php" >(Registrese aqui si aun no lo hizo.)</a></p>
+          </div>
+
+          <div class="">
+              <button class=" center btn-primary btn"  type="submit" >Send</button>
+          </div>
+        </div>
+
+        </form>
 </div>
-
-</form>
   </div>
 
   </body>
--- miPerfil.php
+++ miPerfil.php
@@ -62,25 +62,28 @@ if ($_POST){
      <title>Mi perfil</title>
 
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+     <script src=”prefixfree.min.js” type="text/javascript"></script>
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="css/login.css">
- <link rel="stylesheet" href="css/home.css">
+
+
+ <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/master.css">
+  <link rel="stylesheet" href="css/login.css">
  </head>
 
-  <body >
+  <body style="height: -webkit-fill-available;   display: block;  align-content: center;">
 <?php require_once('partials/header.php') ?>
-<div  class=" containerExt">
-    <div class="containerExt maxViewport styleLogin">
+<div id="padre"  class="contPadreFlex" style="width: 96%;     margin: 2%;" overflow:hidden >
+    <div id="main" class=" styleLogin" style="width: 96%;  padding: 5%;  margin: 2%;" >
 
 
       <div class="containerExt">
 
           <div class="containerDentro ">
             <h1 class="styleTitle center" >Bienvenido! <?=($name.' '.$lastName) ?></h1>
-            <img class="containerDentro  "src="img\avatar\<?=$avatar?>" alt="Yo"style="height:150px; border-radius: 15px 15px 15px 15px;">
+            <img class="containerDentro  "src="img\avatar\<?=$avatar?>" alt="Yo"style="width: -webkit-fill-available; border-radius: 15px 15px 15px 15px;">
           </div>
 
       <form class="" action="miPerfil.php" method="post" enctype="multipart/form-data" >
@@ -91,23 +94,23 @@ if ($_POST){
       </form>
 
 
-        <ul class="center">
-          <li class="center btn-primary btn" style="width:300px;">
+        <ul class="center" style="padding-inline-start: 0px;">
+          <li class=" btn-primary btn">
             Mis Compras
           </li>
-          <li class="center btn-primary btn" style="width:300px;">
+          <li class=" btn-primary btn" >
             Favoritos
           </li>
-          <li class="center btn-primary btn" style="width:300px;">
+          <li class=" btn-primary btn" >
             Presupuestos
           </li>
-          <li class="center btn-primary btn" style="width:300px;">
+          <li class=" btn-primary btn" >
             Envios
           </li>
-          <li class="center btn-primary btn" style="width:300px;">
+          <li class=" btn-primary btn" >
             Mis Datos
           </li>
-          <li class="center btn-primary btn" style="width:300px;">
+          <li class=" btn-primary btn" >
             Seguridad
           </li>
         </ul>
